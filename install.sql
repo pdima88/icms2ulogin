883cc2912ee67be1d5721211712243435ceb8151
Puchkin Dmitriy <pdima88@mail.ru>
2019-07-11 12:16:42 +0500

    update sql

--- install.sql
+++ install.sql
@@ -3,6 +3,9 @@ CREATE TABLE IF NOT EXISTS `{#}ulogin_user` (
   `user_id` int(11) NOT NULL,
   `identity` varchar(255) NOT NULL,
   `network` varchar(50) DEFAULT NULL,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  `data` TEXT NULL,
   PRIMARY KEY (`id`),
   INDEX (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
